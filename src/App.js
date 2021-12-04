b33b5688ce6e6aa6ed721a851ef328f04a08edab
cooky21 <sadinina.95@gmail.com>
2021-12-04 17:58:07 +0100

    making changes through feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi There!
-          Edit <code>src/App.js</code> and save to reload.
+          It took me so much time to be here
+          I was just changed by a feature branch :)
         </p>
         <a
           className="App-link"
